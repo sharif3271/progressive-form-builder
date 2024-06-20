a083b00419f83dc52ce874e181c469919a37c911
sharif eiri <sharif3271@gmail.com>
2024-06-20 15:25:05 +0400

    set initial configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ include: ['lib'] })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    copyPublicDir: false,
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+    }
+  }
 })

94234b66e5c470912f1bd2997e21e2ab1afb9c1e
sharif eiri <sharif3271@gmail.com>
2024-07-26 22:33:47 +0400

    file resolve

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
+      entry: resolve(__dirname, 'lib/main.tsx'),
       formats: ['es']
     },
     copyPublicDir: false,
